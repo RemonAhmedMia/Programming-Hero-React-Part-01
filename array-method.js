2b34e19a6ab194353ce1e9f5edc32905ead50988
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-05 20:16:10 +0100

    map-02

--- array-method.js
+++ array-method.js
@@ -1,3 +1,5 @@
+// Object Array এর সাথে map()
+
 const products =[
     {name: 'laptop', price: 32000, brand: 'lenovo' , color: 'black'},
     {name: 'watch', price: 2000, brand: 'iPhone' , color: 'silver'},
@@ -13,4 +15,29 @@ console.log(brands);
 console.log(prices);
 console.log(names);
 
+// map() হলো JavaScript এর Array Method।
+// এটা দিয়ে তুমি একটা array এর প্রতিটা element এর উপর একই কাজ করে নতুন একটা array তৈরি করতে পারো।
 
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = numbers.map(num => num * 2);
+
+console.log(doubled); // [2, 4, 6, 8, 10]
+console.log(numbers); // [1, 2, 3, 4, 5] (original array change হয়নি)
+//এখানে map() প্রতিটা num কে *2 করে নতুন array বানিয়েছে।
+
+// Array of Strings===
+const persons = ["Remon", "Hasan", "Rafi"];
+
+const upperCase = persons.map(name => name.toUpperCase());
+
+console.log(upperCase); // ["REMON", "HASAN", "RAFI"]
+
+
+//map() এ index ব্যবহার করা যায়==
+const songkha = [10, 20, 30];
+
+const withIndex = songkha.map((num, index) => `Index ${index}: ${num}`);
+
+console.log(withIndex);
+// ["Index 0: 10", "Index 1: 20", "Index 2: 30"]
