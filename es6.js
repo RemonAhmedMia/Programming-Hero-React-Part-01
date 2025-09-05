52151c01d78c91161cff61ea783ccf9ff412f942
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-05 19:21:40 +0100

    first commit

---
+++ es6.js
@@ -0,0 +1,8 @@
+const numbers = [45,67,90,87];
+const students ={
+    name : 'Rimon',
+    age : 23,
+    movies : ['King Khan' , 'Dhakar Sultan'],
+};
+const about = `my name is ${students.name} i am ${students.age} years old, has numbers ${numbers[0]} also favourite movie is ${students.movies[1]}`;
+console.log(about);
