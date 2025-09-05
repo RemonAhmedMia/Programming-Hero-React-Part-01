5e52deaec3e4c89a76f9d5cbb0498cec98d6eb5a
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-05 19:37:43 +0100

    array-function

--- es6.js
+++ es6.js
@@ -6,3 +6,33 @@ const students ={
 };
 const about = `my name is ${students.name} i am ${students.age} years old, has numbers ${numbers[0]} also favourite movie is ${students.movies[1]}`;
 console.log(about);
+
+// =====================
+
+// JavaScript এ function লিখার একটা শর্টকাট পদ্ধতি হলো Arrow Function।
+// এটা => (fat arrow) ব্যবহার করে লেখা হয়।
+
+// সাধারণ ফাংশন (Normal Function)
+function add(a, b) {
+  return a + b;
+}
+console.log(add(5, 3)); // 8
+
+// একই কাজ Arrow Function দিয়ে
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(5, 3)); // 8
+
+// আরও শর্টকাট
+
+// যদি ফাংশনের ভেতরে শুধু return থাকে, তাহলে {} আর return বাদ দেওয়া যায়।
+
+const add = (a, b) => a + b;
+console.log(add(5, 3)); // 8
+
+// এক লাইনের Arrow Function
+// যদি শুধু একটাই প্যারামিটার থাকে, তাহলে () ও বাদ দেওয়া যায়।
+
+const square = x => x * x;
+console.log(square(4)); // 16
